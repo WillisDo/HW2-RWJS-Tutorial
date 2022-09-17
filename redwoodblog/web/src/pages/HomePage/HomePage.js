52f5fd907dcbfb06ed7de4ab9d650c10ab935069
vap49 <vap49@njit.edu>
2022-09-17 13:57:46 -0400

    migration sucessful, scaffolded a CRUD db, created a cell, added cellsto home page, Cells section complete

--- redwoodblog/web/src/pages/HomePage/HomePage.js
+++ redwoodblog/web/src/pages/HomePage/HomePage.js
@@ -1,12 +1,12 @@
 import { MetaTags } from '@redwoodjs/web'
-
+import ArticlesCell from 'src/components/ArticlesCell'
 
 const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
-
      <>This is currently our home page</>
+     <ArticlesCell />
     </>
   )
 }
