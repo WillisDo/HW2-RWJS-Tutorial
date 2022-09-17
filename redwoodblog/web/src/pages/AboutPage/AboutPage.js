633a1c0de8edd029010fa118916ccb6937f831e8
dilipnjit <dc575@njit.edu>
2022-09-17 00:10:04 -0400

    added basic styling from chap 1, created home page and about page

---
+++ redwoodblog/web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,21 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+
+      <h1>AboutPage</h1>
+      <p>
+        Find me in <code>./web/src/pages/AboutPage/AboutPage.js</code>
+      </p>
+      <p>
+        Go back to home page
+        <Link to={routes.home()}> Home</Link>
+      </p>
+    </>
+  )
+}
+
+export default AboutPage
