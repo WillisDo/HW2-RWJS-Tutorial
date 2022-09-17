bcd7c56b29c75d2466e030d93cd6b55c30f5fbb9
wd83 <wd83@njit.edu>
2022-09-17 15:32:09 -0400

    finished chapter 2, fixing issues/bugs

--- redwoodblog/web/src/components/ArticleCell/ArticleCell.js
+++ redwoodblog/web/src/components/ArticleCell/ArticleCell.js
@@ -1,7 +1,12 @@
+import Article from 'src/components/Article'
+
 export const QUERY = gql`
-  query FindArticleQuery($id: Int!) {
-    article: article(id: $id) {
+  query ArticleQuery($id: Int!) {
+    article: post(id: $id) {
       id
+      title
+      body
+      createdAt
     }
   }
 `
@@ -11,9 +16,9 @@ export const Loading = () => <div>Loading...</div>
 export const Empty = () => <div>Empty</div>
 
 export const Failure = ({ error }) => (
-  <div style={{ color: 'red' }}>Error: {error?.message}</div>
+  <div style={{ color: 'red' }}>Error: {error.message}</div>
 )
 
 export const Success = ({ article }) => {
-  return <div>{JSON.stringify(article)}</div>
+  return <Article article={article} />
 }
--- redwoodblog/web/src/pages/ArticlePage/ArticlePage.js
+++ redwoodblog/web/src/pages/ArticlePage/ArticlePage.js
@@ -1,12 +1,12 @@
-import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+import ArticleCell from 'src/components/ArticleCell'
 
-const ArticlePage = ({id}) => {
+const ArticlePage = ({ id }) => {
   return (
     <>
       <MetaTags title="Article" description="Article page" />
-      <ArticleCell id={id} />
 
+      <ArticleCell id={id} />
     </>
   )
 }
