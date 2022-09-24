ebf4dd40a8f6b4ba1dc84410253d24cb16ece571
wd83 <wd83@njit.edu>
2022-09-24 15:07:29 -0400

    finished chapter 1, everything working

---
+++ redwoodblog/web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,15 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+
+      <p>
+        Current iteration of about page
+      </p>
+    </>
+  )
+}
+
+export default AboutPage
